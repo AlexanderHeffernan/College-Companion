fcef8bbdc6b6e6fab9f659224e3da78ccdf9d9b4
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-08-08 22:07:54 +1200

    Initial work for EditItem modal

---
+++ src/components/EditItem.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { modalState } from '../modules/modalState';
+</script>
+<template>
+  <div class="modalBox absolute top-0 left-0">
+    <button @click="modalState.closeModal()">Close</button>
+  </div>
+</template>
