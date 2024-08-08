e237275b15f53474e0f9108755e832e346c387a0
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-08-08 17:05:19 +1200

    Initial setup of project.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import { Task } from './modules/task.js';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  const task = new Task(1, "Assignment 1", "2024-09-30", "Incomplete", 0);
 </script>
 
+<template>
+  <h1>{{task.title}}</h1>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
