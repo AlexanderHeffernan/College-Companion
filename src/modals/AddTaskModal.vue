07fd89d310697b46013dd51e535519460b696c10
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-08-17 20:37:06 +1200

    Bug fix related to Add Task Modal. Add via course ID instead of Course.

--- src/modals/AddTaskModal.vue
+++ src/modals/AddTaskModal.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 import { modalState } from '../modules/modalState';
 import { Task } from '../modules/task';
-import { Course } from '../modules/course';
 import { taskManager } from '../modules/taskManager';
+import { courseManager } from '../modules/courseManager';
 import TaskModal from './templates/TaskModal.vue';
 
-function handleSubmit(name: string, dueDate: string, course: Course | null) {
+function handleSubmit(name: string, dueDate: string, courseId: string | undefined) {
     let newTask = Task.create(name, dueDate);
 
-    if (course) { course.addTask(newTask); }
+    if (courseId) { courseManager.getFromIndex(courseId)?.addTask(newTask); }
 
     taskManager.add(newTask);
     modalState.closeModal();
